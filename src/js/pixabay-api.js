7246461de4b9ee6f5eb4d84451f3df8d2c5cbeed
Yurii Yuriichuk <109101291+YorkAlucard@users.noreply.github.com>
2025-03-02 11:29:12 +0200

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,40 @@
+import { markup } from './render-functions.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { iziOption } from '../main.js';
+
+export function getImage(input) {
+  const box = document.querySelector('.gallery');
+  const API_KEY = '48872034-56219959c0c25d0366fcef29b';
+  const query = encodeURIComponent(input);
+  const urlParams = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+
+  const URL = `https://pixabay.com/api/?${urlParams}`;
+
+  return fetch(URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+
+    .then(data => {
+      return markup(data);
+    })
+    .catch(error => {
+      console.error(error);
+      box.innerHTML = '';
+      iziToast.show({
+        ...iziOption,
+        message: 'Sorry, an error happened. Try again',
+      });
+      return;
+    });
+}
---
+++ src/main.js
@@ -0,0 +1,37 @@
+import { getImage } from './js/pixabay-api.js';
+// import { markup } from './js/render-functions.js';
+import errorIcon from './img/error.svg';
+import closeIcon from './img/close.svg';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+export const iziOption = {
+  messageColor: '#FAFAFB',
+  messageSize: '16px',
+  backgroundColor: '#EF4040',
+  iconUrl: errorIcon,
+  transitionIn: 'bounceInLeft',
+  position: 'topRight',
+  displayMode: 'replace',
+  closeOnClick: true,
+  closeIcon: true,
+  close: closeIcon,
+  class: 'iziToast-red',
+};
+
+document.querySelector('.search-form').addEventListener('submit', event => {
+  const input = document.querySelector('.search-input').value.trim();
+  const box = document.querySelector('.gallery');
+  event.preventDefault();
+
+  if (!input) {
+    iziToast.show({
+      ...iziOption,
+      message: 'Please enter the search query',
+    });
+    return;
+  }
+  box.innerHTML =
+    '<p>Wait, the image is loaded</p><span class="loader"></span>';
+  getImage(input);
+});
